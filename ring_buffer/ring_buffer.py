0893d141f46edbed40290c6bac06e4678ba5dbe7
isaac stott <isaac.cloyd@gmail.com>
2020-07-18 14:40:03 -0600

    got the ring buffer to work.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,24 +1,33 @@
-pointer = 0
-
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.data = []
+        self.pointer = 0
 
     def append(self, item):
-        global pointer
         
-        if pointer >= 5:
-            pointer = 0
+        if self.pointer >= 5:
+            self.pointer = 0
 
         if len(self.data) < self.capacity:
             self.data.append(item)
         else:
             if len(self.data) >= self.capacity:
-                self.data.remove(self.data[pointer])
-                self.data.insert(pointer, item)
-                pointer += 1
+                self.data.remove(self.data[self.pointer])
+                self.data.insert(self.pointer, item)
+                self.pointer += 1
        
     def get(self):
         return self.data
+
+        # if self.pointer >= 5:
+        #     self.pointer = 0
+
+        # if len(self.data) < self.capacity:
+        #     self.data.append(item)
+        # else:
+        #     if len(self.data) >= self.capacity:
+        #         self.data.remove(self.data[self.pointer])
+        #         self.data.insert(self.pointer, item)
+        #         self.pointer += 1
 
